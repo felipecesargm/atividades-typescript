c9b8e02f9b6f0f477588d298508e042445a4cfbd
felipecesargm <felipe.cesar1997@gmail.com>
2022-07-09 13:42:38 -0300

    correção do desafio 3

--- desafio3.ts
+++ desafio3.ts
@@ -1,37 +1,35 @@
 let botaoAtualizar = document.getElementById('atualizar-saldo');
-let botaoLimpar = document.getElementById('limpar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
 let soma = document.getElementById('soma') as HTMLInputElement;
-let campoSaldo = document.getElementById('campo-saldo');
+let campoSaldo = document.getElementById('campo-saldo')!;
 
 let saldoTotal = 0;
 
 limparSaldo()
 
-function somarAoSaldo(soma: number) {
-    if(campoSaldo){
-        saldoTotal += soma;
-        campoSaldo.innerHTML = saldoTotal.toString();
-        limpaCampo();
-    }
+function somarAoSaldo(soma: number) {    
+    saldoTotal += soma;
+    campoSaldo.innerHTML = saldoTotal.toString();
+    limpaCampo();
+    
 }
 
 function limpaCampo() {
     soma.value = "";
 }
 
-function limparSaldo() {
-    if(campoSaldo){
-        saldoTotal = 0;
-        campoSaldo.innerHTML = saldoTotal.toString();
+function limparSaldo() {    
+    saldoTotal = 0;
+    campoSaldo.innerHTML = saldoTotal.toString();
     }
-}
+
 if(botaoAtualizar) {
-    botaoAtualizar.addEventListener('click', function () {
+    botaoAtualizar.addEventListener('click',  () => {
         somarAoSaldo(Number(soma.value));        
     });
 }
 
-botaoLimpar.addEventListener('click', function () {
+botaoLimpar.addEventListener('click',  () => {
     limparSaldo();
 });
 
